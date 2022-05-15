5787d34f658f61ed19661197ee11bc6853532060
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-05-15 10:14:43 +0200

    responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 const Navbar = () => {
 
   const [isNavbarActive, setNavbarActive] = useState(false)
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
 
 
   const handleScroll = () => {
@@ -15,6 +17,12 @@ const Navbar = () => {
       setNavbarActive(false)
     }
   }
+
+  
+  const handleOpenMenu = () => {
+    const sideMenu = document.querySelector('.sideMenu')
+   setMenuOpen(!isMenuOpen)
+ }
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
@@ -27,6 +35,18 @@ const Navbar = () => {
         <li className="navItem"><a href="#about">About</a></li>
         <li className="navItem"><a href="#contact">Contact</a></li>
     </ul>
+    <div className={`${isMenuOpen ? 'navMenu open' : 'navMenu'}`} onClick={handleOpenMenu}>
+        <div className='barTop'></div>
+        <div className='barMid'></div>
+        <div className='barBottom'></div>
+      </div>
+      <div className={`${isMenuOpen ? 'sideNavMenu open' : 'sideNavMenu'}`}>
+        <ul>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#contact'>Contact</a></li>
+        </ul>
+      </div>
 </div>
   )
 }
